ebf718b5bbfc4ae3f8381f653ee04dc96eb13fe0
Jackflyingzzz <105610300+Jackflyingzzz@users.noreply.github.com>
2022-07-30 15:50:28 +0100

    Update script_launch_parallel_cluster.sh

--- RL_UROP-master/Cylinder2DFlowControlWithRL/script_launch_parallel_cluster.sh
+++ RL_UROP-master/Cylinder2DFlowControlWithRL/script_launch_parallel_cluster.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-#PBS -l walltime=24:00:00
+#PBS -l walltime=48:00:00
 #PBS -l select=1:ncpus=32:mem=100gb
 
 # Cluster Environment Setup
@@ -8,26 +8,15 @@ cd $PBS_O_WORKDIR
 module load anaconda3
 module load anaconda3/personal
 source activate fenicsproject
-source activate sb3
 # Check that FIRST_PORT and NUM_PORT variables are set
 # TODO
 
 
-NUM_PORT=6
+NUM_PORT=20
 # check that all ports are free
 #output=$(python3 -c "from utils import bash_check_avail; bash_check_avail($NUM_PORT)")
 
-if [ $output == "T" ]; then
-    echo "Ports available, launch..."
-else
-    if [ $output == "F" ]; then
-        echo "Abort; some ports are not avail"
-        exit 0
-    else
-        echo "wrong output checking ports; abort"
-        exit 1
-    fi
-fi
+
 
 # if I went so far, all ports are free: can launch!
 
