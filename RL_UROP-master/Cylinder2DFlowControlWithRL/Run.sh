2e86e80608e1419464832c0145cf75a994fc7023
Jackflyingzzz <105610300+Jackflyingzzz@users.noreply.github.com>
2022-08-09 22:23:43 +0100

    Update Run.sh

--- RL_UROP-master/Cylinder2DFlowControlWithRL/Run.sh
+++ RL_UROP-master/Cylinder2DFlowControlWithRL/Run.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #PBS -l walltime=24:00:00
-#PBS -l select=1:ncpus=1:mem=40gb
+#PBS -l select=1:ncpus=2:mem=40gb
 cd $PBS_O_WORKDIR
 module load anaconda3
 module load anaconda3/personal
